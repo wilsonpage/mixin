eb10cda44861f6b6c54f47bd5ac5cd5a9958e4bb
Wilson Page <wilsonpage@me.com>
2013-05-22 13:30:08 +0100

    First commit

--- index.js
+++ index.js
@@ -10,20 +10,20 @@ var has = {}.hasOwnProperty;
  */
 
 module.exports = function(main) {
-	var args = arguments;
-	var l = args.length;
-	var i = 1;
-	var src;
-	var key;
+  var args = arguments;
+  var l = args.length;
+  var i = 1;
+  var src;
+  var key;
 
-	while (i++ < l) {
-		src = args[i];
-		for (key in src) {
-			if (has.call(src, key)) {
-				main[prop] = src[prop];
-			}
-		}
-	}
+  while (i++ < l) {
+    src = args[i];
+    for (key in src) {
+      if (has.call(src, key)) {
+        main[prop] = src[prop];
+      }
+    }
+  }
 
   return main;
 },
